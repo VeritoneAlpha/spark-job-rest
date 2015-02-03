e3b6a98359aba2af9b2e729b79a05400f1eaa650
Radu Chilom <radu.chilom@atigeo.com>
2015-02-03 15:36:59 +0200

    Added example project.

---
+++ example/example-job/src/main/scala/com.job/SparkJobImplemented.scala
@@ -0,0 +1,25 @@
+package com.job
+
+import com.typesafe.config.Config
+import org.apache.spark.SparkContext
+import api.{SparkJobInvalid, SparkJobValid, SparkJobValidation, SparkJob}
+
+/**
+ * Created by raduc on 03/11/14.
+ */
+class SparkJobImplemented extends SparkJob
+{
+  override def runJob(sc: SparkContext, jobConfig: Config): Any = {
+
+    val nr = jobConfig.getInt("input")
+
+    val list = Range(0,nr)
+    val rdd = sc.parallelize(list)
+    rdd.count()
+
+  }
+
+  override def validate(sc: SparkContext, config: Config): SparkJobValidation = {
+    if(config.hasPath("input")) SparkJobValid() else SparkJobInvalid("The \"input\" parameter is missing.")
+  }
+}
