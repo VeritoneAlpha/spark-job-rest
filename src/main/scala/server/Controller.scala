8c173df732afce6ffebbe1868343f87a645e134c
Radu Chilom <raduchilom@gmail.com>
2015-04-19 12:15:36 +0300

    Added cors directives.

--- src/main/scala/server/Controller.scala
+++ src/main/scala/server/Controller.scala
@@ -9,7 +9,7 @@ import server.domain.actors.{JobActor, ContextManagerActor, ContextActor}
 import ContextActor.{FailedInit}
 import ContextManagerActor._
 import JobActor._
-import spray.http.{MultipartFormData, MediaTypes, StatusCodes}
+import spray.http._
 import spray.routing.{Route, SimpleRoutingApp}
 import akka.pattern.ask
 import server.domain.actors.getValueFromConfig
@@ -24,7 +24,8 @@ import spray.json.DefaultJsonProtocol._
 /**
  * Created by raduc on 03/11/14.
  */
-  class Controller(config: Config, contextManagerActor: ActorRef, jobManagerActor: ActorRef, jarActor: ActorRef, originalSystem: ActorSystem) extends SimpleRoutingApp{
+  class Controller(config: Config, contextManagerActor: ActorRef, jobManagerActor: ActorRef, jarActor: ActorRef, originalSystem: ActorSystem)
+    extends SimpleRoutingApp with CORSDirectives{
 
   implicit val system = originalSystem
   implicit val timeout = Timeout(60000)
@@ -50,33 +51,43 @@ import spray.json.DefaultJsonProtocol._
       complete{
         "Spark Job Rest is up and running!"
       }
+    } ~ options {
+      corsFilter(List("*"), HttpHeaders.`Access-Control-Allow-Methods`(Seq(HttpMethods.OPTIONS, HttpMethods.GET, HttpMethods.POST, HttpMethods.DELETE))) {
+        complete {
+          "OK"
+        }
+      }
     }
   }
 
   def jobRoute: Route = path("jobs"){
     get {
-      respondWithMediaType(MediaTypes.`application/json`) { ctx =>
-        val resultFuture = jobManagerActor ? GetAllJobsStatus()
-        resultFuture.map {
-          case x: List[Any] => ctx.complete(StatusCodes.OK, x)
-          case e: Throwable => ctx.complete(StatusCodes.InternalServerError, e)
-          case x: String => ctx.complete(StatusCodes.InternalServerError, x)
+      corsFilter(List("*")) {
+        respondWithMediaType(MediaTypes.`application/json`) { ctx =>
+          val resultFuture = jobManagerActor ? GetAllJobsStatus()
+          resultFuture.map {
+            case x: List[Any] => ctx.complete(StatusCodes.OK, x)
+            case e: Throwable => ctx.complete(StatusCodes.InternalServerError, e)
+            case x: String => ctx.complete(StatusCodes.InternalServerError, x)
+          }
         }
       }
     } ~
     get {
       path(Segment) { jobId =>
         parameters('contextName) { contextName =>
-          respondWithMediaType(MediaTypes.`application/json`) { ctx =>
-            val resultFuture = jobManagerActor ? JobStatusEnquiry(contextName, jobId)
-            resultFuture.map {
-              case x: JobRunSuccess => ctx.complete(StatusCodes.OK, resultToTable("Finished", x.result))
-              case x: JobRunError => ctx.complete(StatusCodes.InternalServerError, resultToTable("Error", x.errorMessage))
-              case x: JobStarted => ctx.complete(StatusCodes.OK, resultToTable("Running", ""))
-              case x: JobDoesNotExist => ctx.complete(StatusCodes.BadRequest, "JobId does not exist!")
-              case NoSuchContext => ctx.complete(StatusCodes.BadRequest, "Context does not exist!")
-              case e: Throwable => ctx.complete(StatusCodes.InternalServerError, e)
-              case x: String => ctx.complete(StatusCodes.InternalServerError, x)
+          corsFilter(List("*")) {
+            respondWithMediaType(MediaTypes.`application/json`) { ctx =>
+              val resultFuture = jobManagerActor ? JobStatusEnquiry(contextName, jobId)
+              resultFuture.map {
+                case x: JobRunSuccess => ctx.complete(StatusCodes.OK, resultToTable("Finished", x.result))
+                case x: JobRunError => ctx.complete(StatusCodes.InternalServerError, resultToTable("Error", x.errorMessage))
+                case x: JobStarted => ctx.complete(StatusCodes.OK, resultToTable("Running", ""))
+                case x: JobDoesNotExist => ctx.complete(StatusCodes.BadRequest, "JobId does not exist!")
+                case NoSuchContext => ctx.complete(StatusCodes.BadRequest, "Context does not exist!")
+                case e: Throwable => ctx.complete(StatusCodes.InternalServerError, e)
+                case x: String => ctx.complete(StatusCodes.InternalServerError, x)
+              }
             }
           }
         }
@@ -85,33 +96,45 @@ import spray.json.DefaultJsonProtocol._
     post {
       parameters('runningClass, 'context) { (runningClass, context) =>
         entity(as[String]) { configString =>
-          val config = ConfigFactory.parseString(configString)
-          respondWithMediaType(MediaTypes.`application/json`) { ctx =>
-            val resultFuture = jobManagerActor ? RunJob(runningClass, context, config)
-            resultFuture.map {
-              case x: String => ctx.complete(StatusCodes.OK, x)
-              case e: Error => ctx.complete(StatusCodes.InternalServerError, e)
-              case NoSuchContext => ctx.complete(StatusCodes.BadRequest, "No such context.")
+          corsFilter(List("*")) {
+            val config = ConfigFactory.parseString(configString)
+            respondWithMediaType(MediaTypes.`application/json`) { ctx =>
+              val resultFuture = jobManagerActor ? RunJob(runningClass, context, config)
+              resultFuture.map {
+                case x: String => ctx.complete(StatusCodes.OK, x)
+                case e: Error => ctx.complete(StatusCodes.InternalServerError, e)
+                case NoSuchContext => ctx.complete(StatusCodes.BadRequest, "No such context.")
+              }
             }
           }
         }
       }
-    }
+    } ~
+      options {
+        corsFilter(List("*"), HttpHeaders.`Access-Control-Allow-Methods`(Seq(HttpMethods.OPTIONS, HttpMethods.GET, HttpMethods.POST, HttpMethods.DELETE))) {
+          complete {
+            "OK"
+          }
+        }
+      }
+
   }
 
     def contextRoute : Route = pathPrefix("contexts"){
     post {
       path(Segment) { contextName =>
         entity(as[String]) { configString =>
-          respondWithMediaType(MediaTypes.`application/json`) { ctx =>
+          corsFilter(List("*")) {
+            respondWithMediaType(MediaTypes.`application/json`) { ctx =>
 
-          val config = ConfigFactory.parseString(configString)
+              val config = ConfigFactory.parseString(configString)
 
-          val resultFuture = contextManagerActor ? CreateContext(contextName, getValueFromConfig(config, "jars", ""), config)
-            resultFuture.map {
-              case ContextInitialized(sparkUiPort) => ctx.complete(StatusCodes.OK, sparkUiPort)
-              case e:FailedInit => ctx.complete(StatusCodes.InternalServerError, "Failed Init: " + e.message)
-              case ContextAlreadyExists => ctx.complete(StatusCodes.BadRequest, "Context already exists.")
+              val resultFuture = contextManagerActor ? CreateContext(contextName, getValueFromConfig(config, "jars", ""), config)
+              resultFuture.map {
+                case ContextInitialized(sparkUiPort) => ctx.complete(StatusCodes.OK, sparkUiPort)
+                case e: FailedInit => ctx.complete(StatusCodes.InternalServerError, "Failed Init: " + e.message)
+                case ContextAlreadyExists => ctx.complete(StatusCodes.BadRequest, "Context already exists.")
+              }
             }
           }
         }
@@ -119,16 +142,19 @@ import spray.json.DefaultJsonProtocol._
     } ~
     get {
       path(Segment) { contextName =>
-        val resultFuture = contextManagerActor ? GetContext(contextName)
-        respondWithMediaType(MediaTypes.`application/json`) { ctx =>
-          resultFuture.map{
-            case NoSuchContext => ctx.complete(StatusCodes.BadRequest, "No such context.")
-            case x:ActorSelection => ctx.complete(StatusCodes.OK, "Context exists.")
+        corsFilter(List("*")) {
+          val resultFuture = contextManagerActor ? GetContext(contextName)
+          respondWithMediaType(MediaTypes.`application/json`) { ctx =>
+            resultFuture.map {
+              case NoSuchContext => ctx.complete(StatusCodes.BadRequest, "No such context.")
+              case x: ActorSelection => ctx.complete(StatusCodes.OK, "Context exists.")
+            }
           }
         }
-        }
+      }
     } ~
     get {
+      corsFilter(List("*")) {
         respondWithMediaType(MediaTypes.`application/json`) { ctx =>
           val resultFuture = contextManagerActor ? GetAllContextsForClient()
           resultFuture.map {
@@ -136,15 +162,24 @@ import spray.json.DefaultJsonProtocol._
             case e: Any => ctx.complete(StatusCodes.InternalServerError, e.toString)
           }
         }
+      }
     } ~
     delete {
       path(Segment) { contextName =>
-        val resultFuture = contextManagerActor ? DeleteContext(contextName)
-        respondWithMediaType(MediaTypes.`application/json`) { ctx =>
-          resultFuture.map{
-            case NoSuchContext => ctx.complete(StatusCodes.BadRequest, "No such context.")
-            case Success => ctx.complete(StatusCodes.OK, "Context deleted.")
+        corsFilter(List("*")) {
+          val resultFuture = contextManagerActor ? DeleteContext(contextName)
+          respondWithMediaType(MediaTypes.`application/json`) { ctx =>
+            resultFuture.map {
+              case NoSuchContext => ctx.complete(StatusCodes.BadRequest, "No such context.")
+              case Success => ctx.complete(StatusCodes.OK, "Context deleted.")
+            }
           }
+        }
+      }
+    } ~ options {
+      corsFilter(List("*"), HttpHeaders.`Access-Control-Allow-Methods`(Seq(HttpMethods.OPTIONS, HttpMethods.GET, HttpMethods.POST, HttpMethods.DELETE))) {
+        complete {
+          "OK"
         }
       }
     }
@@ -155,11 +190,13 @@ import spray.json.DefaultJsonProtocol._
     post {
       path(Segment) { jarName =>
         entity(as[Array[Byte]]) { jarBytes =>
-          val resultFuture = jarActor ? AddJar(jarName, jarBytes)
-          respondWithMediaType(MediaTypes.`application/json`) { ctx =>
-            resultFuture.map {
-              case Failure(e) => ctx.complete(StatusCodes.InternalServerError, e.getMessage)
-              case Success(message: String) => ctx.complete(StatusCodes.OK, message)
+          corsFilter(List("*")) {
+            val resultFuture = jarActor ? AddJar(jarName, jarBytes)
+            respondWithMediaType(MediaTypes.`application/json`) { ctx =>
+              resultFuture.map {
+                case Failure(e) => ctx.complete(StatusCodes.InternalServerError, e.getMessage)
+                case Success(message: String) => ctx.complete(StatusCodes.OK, message)
+              }
             }
           }
         }
@@ -167,23 +204,33 @@ import spray.json.DefaultJsonProtocol._
     } ~
     delete {
       path(Segment) { jarName =>
-        val resultFuture = jarActor ? DeleteJar(jarName)
-        respondWithMediaType(MediaTypes.`application/json`) { ctx =>
-          resultFuture.map {
-            case NoSuchJar() => ctx.complete(StatusCodes.BadRequest, "No such jar.")
-            case Success(message: String) => ctx.complete(StatusCodes.OK, message)
-            case x:Exception => ctx.complete(StatusCodes.InternalServerError, x.getMessage)
+        corsFilter(List("*")) {
+          val resultFuture = jarActor ? DeleteJar(jarName)
+          respondWithMediaType(MediaTypes.`application/json`) { ctx =>
+            resultFuture.map {
+              case NoSuchJar() => ctx.complete(StatusCodes.BadRequest, "No such jar.")
+              case Success(message: String) => ctx.complete(StatusCodes.OK, message)
+              case x: Exception => ctx.complete(StatusCodes.InternalServerError, x.getMessage)
+            }
           }
         }
       }
     } ~
     get {
-      respondWithMediaType(MediaTypes.`application/json`) { ctx =>
-        val future = jarActor ? GetAllJars()
-        future.map{
-          case list:List[Any] => {
-            ctx.complete(StatusCodes.OK, list)
+      corsFilter(List("*")) {
+        respondWithMediaType(MediaTypes.`application/json`) { ctx =>
+          val future = jarActor ? GetAllJars()
+          future.map {
+            case list: List[Any] => {
+              ctx.complete(StatusCodes.OK, list)
+            }
           }
+        }
+      }
+    } ~ options {
+      corsFilter(List("*"), HttpHeaders.`Access-Control-Allow-Methods`(Seq(HttpMethods.OPTIONS, HttpMethods.GET, HttpMethods.POST, HttpMethods.DELETE))) {
+        complete {
+          "OK"
         }
       }
     }
