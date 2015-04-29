cfe493a08e697e9a1636a0fe8e683b87f7dae4f2
Radu Chilom <raduchilom@gmail.com>
2015-04-29 10:19:55 +0300

    Updated Settings.sh

--- spark-job-rest/src/main/resources/settings.sh
+++ spark-job-rest/src/main/resources/settings.sh
@@ -1,5 +1,8 @@
 SPARK_HOME="/Users/raduchilom/spark-1.1.0"
 SPARK_CONF_HOME=$SPARK_HOME/conf
+
 # Only needed for Mesos deploys
 #SPARK_EXECUTOR_URI=/home/spark/spark-1.1.0.tar.gz
+
+#Logging directory
 LOG_DIR=/Users/raduchilom/projects/spark-job-rest/logs
