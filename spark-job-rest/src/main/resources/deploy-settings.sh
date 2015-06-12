8bad777e7ec076bdf036573bf4104680b06c80eb
Mykhailo Ziatin <mikhail@datarobot.com>
2015-06-12 19:40:42 +0300

    Ignore deploy files

--- spark-job-rest/src/main/resources/deploy-settings.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-EXTRA_CLASSPATH="${EXTRA_CLASSPATH}:/opt/cloudera/parcels/CDH/jars/hadoop-aws-2.6.0-cdh5.4.2.jar:/opt/cloudera/parcels/CDH/jars/guava-12.0.1.jar"
-
-JAVA_OPTS="${JAVA_OPTS}
-           -Dspray.can.server.parsing.max-content-length=100m"
