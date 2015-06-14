3173ede5f4b6b31ac53a49076794d44493d9dfe4
Mykhailo Ziatin <mikhail@datarobot.com>
2015-06-14 16:49:41 +0300

    Remove deprecated Timeout() in actor utils.

--- spark-job-rest/src/main/scala/server/domain/actors/package.scala
+++ spark-job-rest/src/main/scala/server/domain/actors/package.scala
@@ -4,22 +4,22 @@ import akka.util.Timeout
 import com.typesafe.config.Config
 import org.apache.spark.SparkConf
 
+import scala.concurrent.duration._
 import scala.collection.JavaConverters._
 
 /**
- * Created by raduc on 13/11/14.
+ * Utility functions for actors
  */
 package object actors {
-  implicit val timeout = Timeout(50000)
+  implicit val timeout: Timeout = 50 seconds
 
   def getValueFromConfig[T](config: Config, configPath: String, defaultValue: T): T ={
-    return if (config.hasPath(configPath)) config.getAnyRef(configPath).asInstanceOf[T] else defaultValue
+    if (config.hasPath(configPath)) config.getAnyRef(configPath).asInstanceOf[T] else defaultValue
   }
 
   def configToSparkConf(config:Config, contextName:String, jars: List[String]): SparkConf ={
     val sparkConf = new SparkConf().setAppName(contextName).setJars(jars)
-    for(x <- config.entrySet().asScala
-      if(x.getKey.startsWith("spark."))){
+    for(x <- config.entrySet().asScala if x.getKey.startsWith("spark.")) {
       sparkConf.set(x.getKey, x.getValue.unwrapped().toString)
     }
     sparkConf
